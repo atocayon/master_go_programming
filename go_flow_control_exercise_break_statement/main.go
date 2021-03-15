9d834c8c35c8eec4e60cf57137f1cd76acd14cca
Aljon Tocayon <actwhitehat27@gmail.com>
2021-03-15 13:15:21 +0800

    go exercise: flow control - break statement

---
+++ go_flow_control_exercise_break_statement/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// Using a for loop and an if statement
+// print out all the numbers between 1 and 50 that divisible by 7.
+func main() {
+	count := 0
+	for i := 1; i < 50; i++ {
+
+		if i%7 == 0 {
+			fmt.Println(i)
+			count++
+			continue
+		}
+
+		if count == 3 {
+			break
+		}
+
+	}
+}
